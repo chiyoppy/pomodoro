fa81339998428982954e084a85dd3d3b6662ed45
Hiroshi Chiyokawa <hiroshi.chiyokawa@gmail.com>
2025-05-28 21:52:18 +0900

    Initial Commit

---
+++ pomodoro/pomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoroApp.swift
+//  pomodoro
+//
+//  Created by Hiroshi Chiyokawa on 2025/05/28.
+//
+
+import SwiftUI
+
+@main
+struct pomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pomodoroTests/pomodoroTests.swift
@@ -0,0 +1,17 @@
+//
+//  pomodoroTests.swift
+//  pomodoroTests
+//
+//  Created by Hiroshi Chiyokawa on 2025/05/28.
+//
+
+import Testing
+@testable import pomodoro
+
+struct pomodoroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
